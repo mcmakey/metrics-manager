ff7b18c99bf46ee55da94d596d9195537ea67e42
makey <vadimmakievsky@gmail.com>
2021-06-11 05:17:11 +0300

    Init-project: Добавлен CpuMetricsControllerUnitTests

---
+++ MetricsManagerTests/CpuMetricsControllerUnitTests.cs
@@ -0,0 +1,34 @@
+using System;
+using MetricsManager.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace MetricsManagerTests
+{
+    public class CpuMetricsControllerUnitTests
+    {
+
+        private CpuMetricsController _controller;
+
+        public CpuMetricsControllerUnitTests()
+        {
+            _controller = new CpuMetricsController();
+        }
+
+        [Fact]
+        public void GetMetricsFromAgent_ReturnsOk()
+        {
+            // Arrange
+            var agentId = 1;
+            var fromTime = TimeSpan.FromSeconds(0);
+            var toTime = TimeSpan.FromSeconds(100);
+
+            // Act
+            var result = _controller.GetMetricsFromAgent(agentId, fromTime, toTime);
+
+            // Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+
+        }
+    }
+}
