ffe0e0afcf231f8dfa720100ec74a5e2492504f1
makey <vadimmakievsky@gmail.com>
2021-06-12 20:38:25 +0300

    Lesson-3: Добавлен логгер в CpuMetricsController (CpuMetricsControllerTest не компилится)

--- MetricsManager/Controllers/CpuMetricsController.cs
+++ MetricsManager/Controllers/CpuMetricsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 
 namespace MetricsManager.Controllers
@@ -7,9 +8,18 @@ namespace MetricsManager.Controllers
     [ApiController]
     public class CpuMetricsController : ControllerBase
     {
+        private readonly ILogger<CpuMetricsController> _logger;
+
+        public CpuMetricsController(ILogger<CpuMetricsController> logger)
+        {
+            _logger = logger;
+            _logger.LogDebug(1, "NLog встроен в CpuMetricsController");
+        }
+
         [HttpGet("agent/{agentId}/from/{fromTime}/to/{toTime}")]
         public IActionResult GetMetricsFromAgent([FromRoute] int agentId, [FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime)
         {
+            _logger.LogInformation("Привет! Это наше первое сообщение в лог");
             return Ok("cpu GetMetricsFromAgent");
         }
 
