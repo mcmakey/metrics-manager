43fcb858a8afe2fa7465a4982da55abc74cfa467
makey <vadimmakievsky@gmail.com>
2021-06-14 15:56:25 +0300

    Lesson-3: Создание репозитрия для CpuMetrics (MetricsAgent)

---
+++ MetricsAgent/DAL/CpuMetricsRepository.cs
@@ -0,0 +1,114 @@
+﻿using MetricsAgent.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+
+namespace MetricsAgent.DAL
+{
+    // маркировочный интерфейс
+    // необходим, чтобы проверить работу репозитория на тесте-заглушке
+    public interface ICpuMetricsRepository : IRepository<CpuMetric>
+    {
+
+    }
+
+    public class CpuMetricsRepository : ICpuMetricsRepository
+    {
+        private const string ConnectionString = "Data Source=metrics.db;Version=3;Pooling=true;Max Pool Size=100;";
+
+        public void Create(CpuMetric item)
+        {
+            using var connection = new SQLiteConnection(ConnectionString);
+            connection.Open();
+
+            using var cmd = new SQLiteCommand(connection);
+            cmd.CommandText = "INSERT INTO cpumetrics(value, time) VALUES(@value, @time)";
+            cmd.Parameters.AddWithValue("@value", item.Value);
+            cmd.Parameters.AddWithValue("@time", item.Time.TotalSeconds);
+            cmd.Prepare();
+
+            cmd.ExecuteNonQuery();
+        }
+
+        public void Delete(int id)
+        {
+            using var connection = new SQLiteConnection(ConnectionString);
+            connection.Open();
+
+            using var cmd = new SQLiteCommand(connection);
+            cmd.CommandText = "DELETE FROM cpumetrics WHERE id=@id";
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.Prepare();
+
+            cmd.ExecuteNonQuery();
+        }
+
+        public void Update(CpuMetric item)
+        {
+            using var connection = new SQLiteConnection(ConnectionString);
+            connection.Open(); // в методичке не было
+
+            using var cmd = new SQLiteCommand(connection);
+            cmd.CommandText = "UPDATE cpumetrics SET value = @value, time = @time WHERE id=@id";
+            cmd.Parameters.AddWithValue("@id", item.Id);
+            cmd.Parameters.AddWithValue("@value", item.Value);
+            cmd.Parameters.AddWithValue("@time", item.Time.TotalSeconds);
+            cmd.Prepare();
+
+            cmd.ExecuteNonQuery();
+        }
+
+        public IList<CpuMetric> GetAll()
+        {
+            using var connection = new SQLiteConnection(ConnectionString);
+            connection.Open();
+
+            using var cmd = new SQLiteCommand(connection);
+            cmd.CommandText = "SELECT * FROM cpumetrics";
+
+            var returnList = new List<CpuMetric>();
+
+            using (SQLiteDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    returnList.Add(new CpuMetric
+                    {
+                        Id = reader.GetInt32(0),
+                        Value = reader.GetInt32(1),
+                        Time = TimeSpan.FromSeconds(reader.GetInt32(2))
+                    });
+                }
+            }
+
+            return returnList;
+        }
+
+        public CpuMetric GetById(int id)
+        {
+            using var connection = new SQLiteConnection(ConnectionString);
+            connection.Open();
+
+            using var cmd = new SQLiteCommand(connection);
+            cmd.CommandText = "SELECT * FROM cpumetrics WHERE id=@id";
+            cmd.Parameters.AddWithValue("@id", id); // в методичке не было
+
+            using (SQLiteDataReader reader = cmd.ExecuteReader())
+            {
+                if (reader.Read())
+                {
+                    return new CpuMetric
+                    {
+                        Id = reader.GetInt32(0),
+                        Value = reader.GetInt32(1),
+                        Time = TimeSpan.FromSeconds(reader.GetInt32(1))
+                    };
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
