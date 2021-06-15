5dfd3e4bf3879fb3e1558b0350abbc0c44435e47
makey <vadimmakievsky@gmail.com>
2021-06-15 23:04:22 +0300

    Lesson-3: MetricsAgent - подключение логгера

--- MetricsAgent/Program.cs
+++ MetricsAgent/Program.cs
@@ -1,4 +1,7 @@
+using System;
+using NLog.Web;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Hosting;
 
 namespace MetricsAgent
@@ -7,7 +10,24 @@ namespace MetricsAgent
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            try
+            {
+                logger.Debug("init main");
+                CreateHostBuilder(args).Build().Run();
+            }
+            // отлов всех исключений в рамках работы приложения
+            catch (Exception exception)
+            {
+                //NLog: устанавливаем отлов исключений
+                logger.Error(exception, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                // остановка логера
+                NLog.LogManager.Shutdown();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -15,6 +35,12 @@ namespace MetricsAgent
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+            .ConfigureLogging(logging =>
+            {
+                logging.ClearProviders(); // создание провайдеров логирования
+                logging.SetMinimumLevel(LogLevel.Trace); // устанавливаем минимальный уровень логирования
+
+            }).UseNLog(); // добавляем библиотеку nlog
     }
 }
