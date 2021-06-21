7021fdb0b355784123851832e7a0490be9ca471a
makey <vadimmakievsky@gmail.com>
2021-06-21 08:10:09 +0300

    Lesson-3: Тесты методов "GetAll" и "GetByTimePeriod" для остальных контроллеров в MetricsAgent

--- MetricsAgentTests/DotNetMetricsControllerTest.cs
+++ MetricsAgentTests/DotNetMetricsControllerTest.cs
@@ -3,6 +3,7 @@ using MetricsAgent.DAL;
 using MetricsAgent.Models;
 using MetricsAgent.Requests;
 using Moq;
+using System.Collections.Generic;
 using Xunit;
 
 namespace MetricsAgentTests
@@ -19,7 +20,6 @@ namespace MetricsAgentTests
         }
 
         [Fact]
-
         public void Create_ShouldCall_Create_From_Repository()
         {
             _mock.Setup(repository => repository.Create(It.IsAny<DotNetMetric>())).Verifiable();
@@ -29,6 +29,41 @@ namespace MetricsAgentTests
                 );
 
             _mock.Verify(repository => repository.Create(It.IsAny<DotNetMetric>()), Times.AtMostOnce());
+        }
+
+
+        [Fact]
+        public void GetAll_ShouldCall_From_Repository()
+        {
+            var metrics = new List<DotNetMetric>
+            {
+                new DotNetMetric {Id = 1, Value = 100, Time = 1},
+                new DotNetMetric {Id = 2, Value = 200, Time = 2}
+            };
+
+            _mock.Setup(repository => repository.GetAll()).Returns(metrics);
+
+            var result = _controller.GetAll();
+
+            _mock.Verify(repository => repository.GetAll(), Times.AtMostOnce());
+        }
+
+        [Fact]
+        public void GetByTimePeriod_ShouldCall_From_Repository()
+        {
+            var metrics = new List<DotNetMetric>
+            {
+                new DotNetMetric {Id = 1, Value = 100, Time = 1},
+                new DotNetMetric {Id = 2, Value = 200, Time = 2}
+            };
+
+            _mock.Setup(repository => repository.GetByTimePeriod(It.IsAny<long>(), It.IsAny<long>())).Returns(metrics);
+
+            var result = _controller.GetByTimePeriod(
+                    new DotNetMetricsGetByPeriodRequest { FromTime = 1, ToTime = 2 }
+                );
+
+            _mock.Verify(repository => repository.GetByTimePeriod(It.IsAny<long>(), It.IsAny<long>()), Times.AtMostOnce());
         }
     }
 }
--- MetricsAgentTests/RamMetricsControllerTest.cs
+++ MetricsAgentTests/RamMetricsControllerTest.cs
@@ -3,6 +3,7 @@ using MetricsAgent.DAL;
 using MetricsAgent.Models;
 using MetricsAgent.Requests;
 using Moq;
+using System.Collections.Generic;
 using Xunit;
 
 namespace MetricsAgentTests
@@ -28,6 +29,40 @@ namespace MetricsAgentTests
                 );
 
             _mock.Verify(repository => repository.Create(It.IsAny<RamMetric>()), Times.AtMostOnce());
+        }
+
+        [Fact]
+        public void GetAll_ShouldCall_From_Repository()
+        {
+            var metrics = new List<RamMetric>
+            {
+                new RamMetric {Id = 1, Value = 100, Time = 1},
+                new RamMetric {Id = 2, Value = 200, Time = 2}
+            };
+
+            _mock.Setup(repository => repository.GetAll()).Returns(metrics);
+
+            var result = _controller.GetAll();
+
+            _mock.Verify(repository => repository.GetAll(), Times.AtMostOnce());
+        }
+
+        [Fact]
+        public void GetByTimePeriod_ShouldCall_From_Repository()
+        {
+            var metrics = new List<RamMetric>
+            {
+                new RamMetric {Id = 1, Value = 100, Time = 1},
+                new RamMetric {Id = 2, Value = 200, Time = 2}
+            };
+
+            _mock.Setup(repository => repository.GetByTimePeriod(It.IsAny<long>(), It.IsAny<long>())).Returns(metrics);
+
+            var result = _controller.GetByTimePeriod(
+                    new RamMetricsGetByPeriodRequest { FromTime = 1, ToTime = 2 }
+                );
+
+            _mock.Verify(repository => repository.GetByTimePeriod(It.IsAny<long>(), It.IsAny<long>()), Times.AtMostOnce());
         }
     }
 }
