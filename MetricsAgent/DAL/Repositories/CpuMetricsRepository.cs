245a808c479e159c1115404b88ac12ccad2b1403
makey <vadimmakievsky@gmail.com>
2021-07-04 16:16:00 +0300

    Lesson-4: using Dapper in CpuMetricsRepository

--- MetricsAgent/DAL/Repositories/CpuMetricsRepository.cs
+++ MetricsAgent/DAL/Repositories/CpuMetricsRepository.cs
@@ -1,7 +1,8 @@
-﻿using MetricsAgent.DAL.Interfaces;
+﻿using Dapper;
+using MetricsAgent.DAL.Interfaces;
 using MetricsAgent.Models;
 using System.Collections.Generic;
-using System.Data.SQLite;
+using System.Linq;
 
 namespace MetricsAgent.DAL
 {
@@ -13,70 +14,31 @@ namespace MetricsAgent.DAL
         {
             using var connection = _connectionManager.CreateOpenedConnection();
 
-            connection.Open();
-
-            using var cmd = new SQLiteCommand(connection);
-            cmd.CommandText = "INSERT INTO cpumetrics(value, time) VALUES(@value, @time)";
-            cmd.Parameters.AddWithValue("@value", item.Value);
-            cmd.Parameters.AddWithValue("@time", item.Time);
-            cmd.Prepare();
-
-            cmd.ExecuteNonQuery();
+            connection.Execute("INSERT INTO cpumetrics(value, time) VALUES(@value, @time)",
+                new
+                {
+                    value = item.Value,
+                    time = item.Time
+                });
         }
 
         public IList<CpuMetric> GetAll()
         {
             using var connection = _connectionManager.CreateOpenedConnection();
-            connection.Open();
 
-            using var cmd = new SQLiteCommand(connection);
-            cmd.CommandText = "SELECT * FROM cpumetrics";
-
-            var returnList = new List<CpuMetric>();
-
-            using (SQLiteDataReader reader = cmd.ExecuteReader())
-            {
-                while (reader.Read())
-                {
-                    returnList.Add(new CpuMetric
-                    {
-                        Id = reader.GetInt32(0),
-                        Value = reader.GetInt32(1),
-                        Time = reader.GetInt64(1)
-                    });
-                }
-            }
-
-            return returnList;
+            return connection.Query<CpuMetric>("SELECT * FROM cpumetrics").ToList();
         }
 
         public IList<CpuMetric> GetByTimePeriod(long fromTime, long toTime)
         {
             using var connection = _connectionManager.CreateOpenedConnection();
-            connection.Open();
 
-            using var cmd = new SQLiteCommand(connection);
-            cmd.CommandText = "SELECT * FROM cpumetrics WHERE time >= @fromTime AND time <= @toTime";
-            cmd.Parameters.AddWithValue("@fromTime", fromTime);
-            cmd.Parameters.AddWithValue("@toTime", toTime);
-            cmd.Prepare();
-
-            var result = new List<CpuMetric>();
-
-            using (SQLiteDataReader reader = cmd.ExecuteReader())
-            {
-                while (reader.Read())
+            return connection.Query<CpuMetric>("SELECT * FROM cpumetrics WHERE time >= @fromTime AND time <= @toTime",
+                new
                 {
-                    result.Add(new CpuMetric
-                    {
-                        Id = reader.GetInt32(0),
-                        Value = reader.GetInt32(1),
-                        Time = reader.GetInt64(1)
-                    });
-                }
-            }
-
-            return result;
+                    fromTime = fromTime,
+                    toTime = toTime
+                }).ToList();
         }
     }
 }
