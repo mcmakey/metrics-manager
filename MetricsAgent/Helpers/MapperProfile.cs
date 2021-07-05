f8d69ad2eb45cfb368a7c20dde0eb4f6e0f85ca5
makey <vadimmakievsky@gmail.com>
2021-07-05 06:34:55 +0300

    Lesson-4: Подключение AutoMapper.

---
+++ MetricsAgent/Helpers/MapperProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MetricsAgent.DTO;
+using MetricsAgent.Models;
+
+namespace MetricsAgent.Helpers
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<CpuMetric, CpuMetricDto>();
+            CreateMap<DotNetMetric, DotNetMetricDto>();
+            CreateMap<HddMetric, HddMetricDto>();
+            CreateMap<NetworkMetric, NetworkMetricDto>();
+            CreateMap<RamMetric, RamMetricDto>();
+        }
+    }
+
+}
